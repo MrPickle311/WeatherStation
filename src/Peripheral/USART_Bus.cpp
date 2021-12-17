b8c387e17c099c948bcac859af5ea0fdb65604ec
MrPickle311 <macmacroll@gmail.com>
2021-12-17 18:17:51 +0100

    USART has been refactored and responsibilites have been decoupled

--- src/Peripheral/USART_Bus.cpp
+++ src/Peripheral/USART_Bus.cpp
@@ -2,89 +2,100 @@
 
 #include <cmath>
 
-namespace Device
+namespace Peripheral
 {
-USART_Bus::USART_Bus(volatile USART_TypeDef* usart):
-		usart_{usart}
+USART_Bus_Base::USART_Bus_Base(USART_TypeDef* usart) : usart_{usart} {}
+
+
+void USART_Bus_Base::resetControlRegisters()
 {
-	resetControlRegisters();
+    CLEAR_REG(usart_->CR1);
+    CLEAR_REG(usart_->CR2);
+    CLEAR_REG(usart_->CR3);
 }
 
-void USART_Bus::resetControlRegisters()
+void USART_Bus_Base::waitForFlagSet(uint32_t flag)
 {
-	CLEAR_REG(usart_->CR1);
-	CLEAR_REG(usart_->CR2);
-	CLEAR_REG(usart_->CR3);
+    while (!(usart_->SR & flag)) // wait until buffer ready
+    {
+        asm volatile("nop");
+    }
 }
 
-void USART_Bus::waitForFlagSet(uint32_t flag)
+USART_Bus_ConfigController::USART_Bus_ConfigController(USART_TypeDef* usart) :
+    base_{USART_Bus_Base::get(usart)}
 {
-	while (! ( usart_->SR & flag ) ) // wait until buffer ready
-	{
-		asm volatile ("nop");
-	}
-}
-
-void USART_Bus::enableBus()
-{
-	usart_->CR1 |= USART_CR1_UE;
-}
-
-void USART_Bus::enableTransmitter()
-{
-	usart_->CR1 |=  USART_CR1_TE ;  // enable rx and tx
-}
-
-void USART_Bus::enableReceiver()
-{
-	usart_->CR1 |=  USART_CR1_RE ;  // enable rx and tx
-}
-
-void USART_Bus::enableDMAForTransmitter()
-{
-	usart_->CR3 |= USART_CR3_DMAT; // enable dma mode
-}
-
-void USART_Bus::enableDMAForReceiver()
-{
-	usart_->CR3 |= USART_CR3_DMAR;
-}
-
-void USART_Bus::setupBaudRate(uint32_t baud_value, uint32_t peripheral_frequency)
-{
-	double temp_result { peripheral_frequency / ( baud_value * 16 ) };
-
-	uint16_t mantisa { static_cast<uint16_t>( std::trunc(temp_result) ) };
-
-	uint8_t fraction { static_cast<uint8_t>( std::round( ( temp_result - mantisa ) * 16 ) )  };
-
-	usart_->BRR = (mantisa << 4) | (fraction << 0);
+    base_.resetControlRegisters();
 }
 
 
-void USART_Bus::sendByte(uint8_t byte)
+void USART_Bus_ConfigController::enableBus()
 {
-	waitForFlagSet(USART_SR_TXE);
-
-	usart_->DR = byte;
+    base_.usart_->CR1 |= USART_CR1_UE;
 }
 
-uint8_t USART_Bus::getByte()
+void USART_Bus_ConfigController::enableTransmitter()
 {
-	waitForFlagSet(USART_SR_RXNE);
-
-	return usart_->DR;
+    base_.usart_->CR1 |= USART_CR1_TE; // enable rx and tx
 }
 
-void USART_Bus::printText(uint8_t* txt)
+void USART_Bus_ConfigController::enableReceiver()
 {
-	while(*txt)
-	{
-		sendByte(*txt);
-		++txt;
-	}
-	sendByte('\r');
-	sendByte('\n');
+    base_.usart_->CR1 |= USART_CR1_RE; // enable rx and tx
 }
 
+void USART_Bus_ConfigController::enableDMAForTransmitter()
+{
+    base_.usart_->CR3 |= USART_CR3_DMAT; // enable dma mode
 }
+
+void USART_Bus_ConfigController::enableDMAForReceiver()
+{
+    base_.usart_->CR3 |= USART_CR3_DMAR;
+}
+
+void USART_Bus_ConfigController::setupBaudRate(uint32_t baud_value,
+                                               uint32_t peripheral_frequency)
+{
+    double temp_result{peripheral_frequency / (baud_value * 16)};
+
+    uint16_t mantisa{static_cast<uint16_t>(std::trunc(temp_result))};
+
+    uint8_t fraction{
+        static_cast<uint8_t>(std::round((temp_result - mantisa) * 16))};
+
+    base_.usart_->BRR = (mantisa << 4) | (fraction << 0);
+}
+
+
+void USART_Bus_IOController::sendByte(uint8_t byte)
+{
+    base_.waitForFlagSet(USART_SR_TXE);
+
+    base_.usart_->DR = byte;
+}
+
+uint8_t USART_Bus_IOController::getByte()
+{
+    base_.waitForFlagSet(USART_SR_RXNE);
+
+    return base_.usart_->DR;
+}
+
+
+USART_Bus_IOController::USART_Bus_IOController(USART_TypeDef* usart) :
+    base_{USART_Bus_Base::get(usart)}
+{}
+
+// void USART_Bus::printText(uint8_t* txt)
+//{
+//    while (*txt)
+//    {
+//        sendByte(*txt);
+//        ++txt;
+//    }
+//    sendByte('\r');
+//    sendByte('\n');
+//}
+
+} // namespace Peripheral
--- src/Peripheral/USART_Bus.hpp
+++ src/Peripheral/USART_Bus.hpp
@@ -3,12 +3,15 @@
 #include "../TypeTraits/Singleton.hpp"
 #include "stm32f1xx.h"
 #include "stm32f1xx_nucleo.h"
-namespace Device
+namespace Peripheral
 {
 
-class USART_Bus : public Multiton<USART_Bus, USART_TypeDef*>
+class USART_Bus_Base :
+    public TypeTraits::Multiton<USART_Bus_Base, USART_TypeDef*>
 {
-    friend class Multiton<USART_Bus, USART_TypeDef*>;
+    friend class Multiton<USART_Bus_Base, USART_TypeDef*>;
+    friend class USART_Bus_ConfigController;
+    friend class USART_Bus_IOController;
 
 private:
     volatile USART_TypeDef* usart_;
@@ -18,7 +21,20 @@ private:
     void waitForFlagSet(uint32_t flag);
 
 private:
-    USART_Bus(volatile USART_TypeDef* usart);
+    USART_Bus_Base(USART_TypeDef* usart);
+};
+
+
+class USART_Bus_ConfigController :
+    public TypeTraits::Multiton<USART_Bus_ConfigController, USART_TypeDef*>
+{
+    friend class Multiton<USART_Bus_ConfigController, USART_TypeDef*>;
+
+private:
+    USART_Bus_Base& base_;
+
+private:
+    USART_Bus_ConfigController(USART_TypeDef* usart);
 
 public:
     void enableBus();
@@ -30,12 +46,25 @@ public:
     void enableDMAForReceiver();
 
     void setupBaudRate(uint32_t baud_value, uint32_t peripheral_frequency);
-
-    void sendByte(uint8_t byte);
-    uint8_t getByte();
-    void sendBytes(uint8_t* bytes, uint16_t count);
-    void printText(uint8_t* text);
 };
 
+class USART_Bus_IOController :
+    public TypeTraits::Multiton<USART_Bus_IOController, USART_TypeDef*>
+{
 
-} // namespace Device
+    friend class Multiton<USART_Bus_ConfigController, USART_TypeDef*>;
+
+private:
+    USART_Bus_Base& base_;
+
+private:
+    USART_Bus_IOController(USART_TypeDef* usart);
+
+public:
+    void sendByte(uint8_t byte);
+    uint8_t getByte();
+    //    void sendBytes(uint8_t* bytes, uint16_t count);
+    //    void printText(uint8_t* text);
+};
+
+} // namespace Peripheral
